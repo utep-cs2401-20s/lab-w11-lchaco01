b75b079d0c9643b5b4858d6687e566c73e15619f
lchaco01 <Darklord00!>
2020-04-10 22:58:28 -0600

    Completed code Completed test cases. added documentation

--- lab7/src/NewSorting.java
+++ lab7/src/NewSorting.java
@@ -1,30 +1,47 @@
 public class NewSorting {
     public void newSorting(int[] a, int size) {
+        /*checks if the array is less than or equal to the given size*/
         if (a.length <= size) {
+            /*start and end are given a value and sent to quickSort*/
             int start = 0;
             int end = a.length - 1;
             quickSort(a,start,end);
         } else {
+            /*cuts the array to be able to use quickSort*/
             int mid = a.length / 2;
+
+            /*creates a new array called left*/
             int[] left = new int[mid];
+
+            /*creates a new array called right*/
             int[] right = new int[a.length - mid];
 
+            /*populates the left array*/
             for (int i = 0; i < left.length; i++) {
                 left[i] = a[i];
             }
 
+            /*populates the right array*/
             for (int i = 0; i < right.length; i++) {
                 right[i] = a[mid + i];
             }
+
+            /*calls newSorting to see if left array and right array fits the size*/
             newSorting(left, size);
             newSorting(right, size);
-            mergeSortHalves(a, left, right);
+
+            /*calls mergeSortedHalves after the arrays are within the size and have gone through quickSort*/
+            mergeSortedHalves(a, left, right);
         }
     }
 
     private int part(int []a, int start, int end) {
+        /*set pivot the beginning of the array*/
         int pivot = a[start];
+
+        /*goes through the given array*/
         for (int i = start; i < end + 1; i++) {
+            /*checks to see if pivot is the larger number and swaps it while moving the start point to put the array in order*/
             if (a[i] < pivot) {
                 int temp = a[i];
                 a[i] = pivot;
@@ -41,38 +58,46 @@ public class NewSorting {
     }
      private void quickSort(int [] a, int start, int end) {
 
-        //base case
+        /*base case*/
         if (start < end && start >= 0) {
+            /*while true it sends the array to part to be ordered*/
             int index = part(a, start, end);
-            //left
+            /*left array is sent to quickSort*/
             quickSort(a,start,index - 1);
-            //right
+            /*right array is sent to quickSort*/
             quickSort(a,index + 1, end);
         }
 
      }
 
-     private void mergeSortHalves(int [] a, int[] left, int [] right) {
+     private void mergeSortedHalves(int [] a, int[] left, int [] right) {
+        /*sets the points for the left and right arrays*/
         int lPoint = 0;
         int rPoint =  0;
 
+        /*goes through both arrays*/
         for(int i = 0; lPoint < left.length || rPoint < right.length; i++) {
+            /*if the left array reaches the end it places the right array element in the main array*/
             if (lPoint == left.length) {
                 a[i] = right[rPoint];
                 rPoint++;
                 continue;
             }
+            /*if the right array reaches the end it places the left array element in the main array*/
             if (rPoint == right.length) {
                 a[i] = left[lPoint];
                 lPoint++;
                 continue;
             }
+            /*if the element in the left array is smaller, it gets placed in the main array*/
             if (left[lPoint] < right[rPoint]) {
                 a[i] = left[lPoint];
                 lPoint++;
+                /*if the elements in both arrays are equal, it places the element for the left array to the main array*/
             } else if (left[lPoint] == right[rPoint]) {
                 a[i] = left[lPoint];
                 lPoint++;
+                /*if the element in the right array is smaller, it gets placed in the main array*/
             } else {
                 a[i] = right[rPoint];
                 rPoint++;
--- lab7/src/NewSortingTest.java
+++ lab7/src/NewSortingTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NewSortingTest {
-
+/*All tests have a different size for testing purposes*/
     @Test
     public void testNewSort() {
         int[] test = {2,5,3,1,4,6};
@@ -11,11 +11,12 @@ class NewSortingTest {
         int[] exp = {1,2,3,4,5,6};
         NewSorting ns = new NewSorting();
         ns.newSorting(test,size);
-        for(int i = 0; i < test.length; i++) {
-            System.out.print(test[i] + " ");
-        }
+//        for(int i = 0; i < test.length; i++) {
+//            System.out.print(test[i] + " ");
+//        }
         assertArrayEquals(exp, test);
     }
+    /* This test ensures that the program can run with regular number, the test passes and provides the expected array */
 
     @Test
     public void testNewSort2() {
@@ -24,11 +25,12 @@ class NewSortingTest {
         int[] exp = {-6,-5,-4,-3,-2,-1};
         NewSorting ns = new NewSorting();
         ns.newSorting(test2,size);
-        for(int i = 0; i < test2.length; i++) {
-            System.out.print(test2[i] + " ");
-        }
+//        for(int i = 0; i < test2.length; i++) {
+//            System.out.print(test2[i] + " ");
+//        }
         assertArrayEquals(exp,test2);
     }
+    /*This test was designed to test an array of negative numbers, the test passes and the expected array is provided.*/
 
     @Test
     public void testNewSort3() {
@@ -37,11 +39,12 @@ class NewSortingTest {
         int[] exp = {-7,-3,-1,4,5,6};
         NewSorting ns = new NewSorting();
         ns.newSorting(test3,size);
-        for(int i = 0; i < test3.length; i++) {
-            System.out.print(test3[i] + " ");
-        }
+//        for(int i = 0; i < test3.length; i++) {
+//            System.out.print(test3[i] + " ");
+//        }
         assertArrayEquals(exp,test3);
     }
+    /*This test is designed to use an array that has both positive and negative numbers, the test passes and the expected array is provided. */
     @Test
     public void testNewSort4() {
         int[] test4 = {12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
@@ -49,11 +52,12 @@ class NewSortingTest {
         int[] exp = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
         NewSorting ns = new NewSorting();
         ns.newSorting(test4, size);
-        for (int i = 0; i < test4.length; i++) {
-            System.out.print(test4[i] + " ");
-        }
+//        for (int i = 0; i < test4.length; i++) {
+//            System.out.print(test4[i] + " ");
+//        }
         assertArrayEquals(exp, test4);
     }
+    /*This test is designed to use a bigger array, the test passes, and the expected array is given.*/
     @Test
     public void testNewSort5() {
         int[] test5 = {101,85,1000,4,75,-100,50,7,32,-5,21,3};
@@ -61,9 +65,36 @@ class NewSortingTest {
         int[] exp = {-100,-5,3,4,7,21,32,50,75,85,101,1000};
         NewSorting ns = new NewSorting();
         ns.newSorting(test5, size);
-        for (int i = 0; i < test5.length; i++) {
-            System.out.print(test5[i] + " ");
-        }
+//        for (int i = 0; i < test5.length; i++) {
+//            System.out.print(test5[i] + " ");
+//        }
         assertArrayEquals(exp, test5);
     }
+    /*This test is designed to use an array that has a wider ranged numbers, the test passes and the expected array is provided.*/
+    @Test
+    public void testNewSort6() {
+        int[] test6 = {1,3,2,4,2,3,4,1};
+        int size = 2;
+        int[] exp = {1,1,2,2,3,3,4,4};
+        NewSorting ns = new NewSorting();
+        ns.newSorting(test6, size);
+//        for (int i = 0; i < test6.length; i++) {
+//            System.out.print(test6[i] + " ");
+//        }
+        assertArrayEquals(exp, test6);
+    }
+    /*This test uses an array with duplicate numbers, the test passes and the expected array is provided.*/
+    @Test
+    public void testNewSort7() {
+        int[] test7 = {0,0};
+        int size = 5;
+        int[] exp = {0,0};
+        NewSorting ns = new NewSorting();
+        ns.newSorting(test7, size);
+//        for (int i = 0; i < test7.length; i++) {
+//            System.out.print(test7[i] + " ");
+//        }
+        assertArrayEquals(exp, test7);
+    }
+    /*This test uses an array with only zeros and a size bigger than the array, the test passes and provides the expected array.*/
 }
